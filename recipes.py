b747aa3390261ec13abc4182c0a45ecda577ecf3
Saud Zahir <mzahir.bee19seecs@seecs.edu.pk>
2023-07-13 12:05:20 +0500

    Update recipes.py

--- recipes.py
+++ recipes.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import re
 
+
 file_path = "Recipes/recipes_data.txt"
+
+
 
 class Recipes:
     def __init__(self, recipes):
@@ -48,7 +54,8 @@ class Recipes:
 
 
 
-    def write_recipes(self, serial_title = "Combinations", separator = "-", text_position = "center"):
+    def get_recipes(self, serial_title = "Combinations", column_separator = "|", separator = "-",
+                      text_position = "center", left_padding = 0, right_padding = 0, column_width = 0):
         
         path = "Recipes/"
         for name in self.names:
@@ -56,57 +63,23 @@ class Recipes:
             ingredients = self.ingredients[name]
             with open(file_name, 'w') as f:
                 headers = [serial_title]
-                for ingredient in ingredients.keys():
-                    headers.append(ingredient)
+                for ingredient_names in ingredients.keys():
+                    headers.append(ingredient_names)
                 
-                # Set the cell width size.
-                if len(serial_title) < 5:
-                    offset  = 15
-                else:
-                    offset = 4
-                    
-                max_cell_length = len(headers[0]) + offset
-                number_of_cells = len(headers)
+                title = name + " Recipes."
+                f.write(f"{title: ^{len(title)}}" + "\n")
+                self.write_combinations(f, headers, ingredients, column_separator, separator, text_position, left_padding, right_padding, column_width)
 
-                for a in range(len(headers)):
-                    header = headers[a]
-                    
-                    # Number of white spaces in a cell.
-                    spacing = max_cell_length - len(header)
 
-                    if a == 0:
-                        # Filling the first cell.
-                        title = "|" + self.get_aligned_text(header, spacing, text_position) + "|"
-                        f.write(title)
-                    else:
-                        title = self.get_aligned_text(header, spacing, text_position) + "|"
-                        f.write(title)
 
-                f.write("\n")
-                f.write(self.get_separators(max_cell_length, number_of_cells, separator))
-                f.write("\n")
-                f.write(self.get_combination(ingredients, max_cell_length, number_of_cells, text_position))
                 
-                # Writing ending line of table.
-                f.write("\n")
-                f.write(self.get_separators(max_cell_length, number_of_cells, separator, add_bar = False))
-                # f.write("\n")
-                # f.write(self.get_separators(max_cell_length, number_of_cells, separator = "_", add_bar = True))
-
-
-
-    def get_combination(self, ingredients, max_cell_length, number_of_cells, text_position):
+    def get_combinations(self, ingredients, number_of_cells):
         """
         This function returns the different
         possible combinations of ingredients
         for each recipe.
         """
         keys = list(ingredients.keys())
-
-        # Calculating the number of rows (or number of possible combinations).
-        N = 1
-        for key in keys:
-            N *= len(ingredients[key])
 
         options = [ingredients[k] for k in keys]
         k = len(keys)
@@ -137,92 +110,154 @@ class Recipes:
         n = number_of_cells - 1
         combinations = [combinations[i * n:(i + 1) * n] for i in range((len(combinations) + n - 1) // n )]
 
+        return combinations
 
-        combination_text = ""
 
+
+
+    def write_combinations(self, file, headers, ingredients, column_separator, separator, text_position, left_padding, right_padding, column_width):
+        """
+        This function writes the different
+        possible combinations of ingredients
+        for each recipe.
+        """
+        
+        number_of_cells = len(headers)
+        
+        combinations = self.get_combinations(ingredients, len(headers))
+        
         for n, combination in enumerate(combinations, start = 1):
             combination.insert(0, str(n))
+        
+        l = left_padding
+        r = right_padding
+        max_cell_lengths = [max([len(h), len(c)]) + l + r for h, c in zip(headers, combinations[:][-1])]
+        
+        for max_cell_length in max_cell_lengths:
+            if column_width != 0 and column_width < max_cell_length:
+                raise Exception("Column width should be greater than or equal to the maximum cell")
+
+        
+        # Calculating the number of rows (or number of possible combinations).
+        N = 1
+        for key in list(ingredients.keys()):
+            N *= len(ingredients[key])
+            
+        combination_text = ""
+        
+        for combination in combinations:
             for i in range(len(combination)):
                 string = combination[i]
+                
+                max_cell_length = column_width if column_width != 0 else max_cell_lengths[i]
                 spacing = max_cell_length - len(string)
-
+                
                 if i == 0:
                     # Filling the first cell.
-                    combination_text += "|" + self.get_aligned_text(string, spacing, text_position) + "|"
+                    combination_text += column_separator + self.get_aligned_text(string, spacing, text_position, l, r) + column_separator
                 else:
-                    combination_text += self.get_aligned_text(string, spacing, text_position) + "|"
+                    combination_text += self.get_aligned_text(string, spacing, text_position, l, r) + column_separator
 
             # Condition for adding a newline untill the last entry.
-            if int(n) < N:
+            if int(combination[0]) < N:
                 combination_text += "\n"
-
                 
-        # combination_text = ""
-        # separator_line = "|"
-        # for n in range(1, N + 1):
-        #     n = str(n)
-        #     spacing = max_cell_length - len(n)
-        #     combination_text += separator_line + self.get_aligned_text(n, spacing, text_position) + "|"
-        #     if int(n) < N:
-        #        combination_text += "\n"
-
-        return combination_text
-
-
-
-
-    def get_aligned_text(self, text, spacing, text_position = "center"):
+                
+        file.write(self.get_separators(max_cell_lengths, number_of_cells, column_width, column_separator, separator, add_column_separation = False))
+        file.write("\n")
+        
+        for a in range(len(headers)):
+            header = headers[a]
+            
+            # Number of white spaces in a cell.
+            max_cell_length = column_width if column_width != 0 else max_cell_lengths[a]
+            spacing = max_cell_length - len(header)
+            
+            # print(f"{header}, {len(header)}, {max_cell_length}, {spacing}")
+            
+            if a == 0:
+                # Filling the first cell.
+                title = column_separator + self.get_aligned_text(header, spacing, text_position, l, r) + column_separator
+                file.write(title)
+                
+            else:
+                title = self.get_aligned_text(header, spacing, text_position, l, r) + column_separator
+                file.write(title)
+        
+        file.write("\n")
+        file.write(self.get_separators(max_cell_lengths, number_of_cells, column_width, column_separator, separator))
+        file.write("\n")
+        
+        file.write(combination_text)
+        
+        # Writing ending line of table.
+        file.write("\n")
+        file.write(self.get_separators(max_cell_lengths, number_of_cells, column_width, column_separator, separator, add_column_separation = False))
+        
+        
+        
+    
+    def check_recipes(self):
+        pass
+    
+    
+    
+    
+    def get_aligned_text(self, text, spacing, text_position = "center", l = 0, r = 0):
         """
         This function sets the alignment
         of the text and returns aligned text.
         """
         
         if text_position == "left":
-            aligned_text = text + " " * spacing
+            spacing = spacing - l - r if l or r != 0 else spacing
+            d = spacing + len(text)
+            aligned_text = " " * l + f"{text : <{d}}" + " " * r
             
         elif text_position == "center":
-            # This is not working. Using f-string method to align at center.
-            # aligned_text = " " * int(spacing/2) + text + " " * int(spacing/2)
-            
+            spacing = spacing - l - r if l or r != 0 else spacing
             d = spacing + len(text)
-            aligned_text = f"{text : ^{d}}"
+            aligned_text = " " * l + f"{text : ^{d}}" + " " * r
             
-        elif text_position == "right":
-            aligned_text = " " * spacing + text
+        elif text_position == "right":        
+            spacing = spacing - l - r if l or r != 0 else spacing
+            d = spacing + len(text)
+            aligned_text = " " * l + f"{text : >{d}}" + " " * r
             
         else:
-            print("Enter the correct alignment.")
+            raise ValueError("Enter the correct alignment. The correct alignments are left, center and right.")
 
         return aligned_text
 
 
 
 
-    def get_separators(self, max_cell_length, number_of_cells, separator = "-", add_bar = True):
+    def get_separators(self, max_cell_lengths, number_of_cells, column_width, column_separator, separator = "-", add_column_separation = True):
         """
         This function returns a separator line
         (a separation between headers and table).
         """
         
-        if add_bar:
-            separator_line = "|"
-            separators = separator * max_cell_length
+        max_cell_lengths = [column_width] * number_of_cells if column_width != 0 else max_cell_lengths
+        
+        if add_column_separation:
+            separator_line = column_separator
             for i in range(number_of_cells):
-                separator_line += separators + "|"
+                separator_line += separator * max_cell_lengths[i] + column_separator
                 
         else:
-            # separator_line = separator * (max_cell_length + 2) * (number_of_cells - 1)
             # Endline.
             separator_line = "-"
-            separators = separator * (max_cell_length + 1)
             for i in range(number_of_cells):
-                separator_line += separators
+                separator_line += separator * (max_cell_lengths[i] + 1)
 
         return separator_line
+
 
 
 
 recipes = Recipes(file_path)
 recipes.get_recipe_names()
 recipes.get_ingredients()
-recipes.write_recipes(serial_title = "N", separator = "-", text_position = "center")
+recipes.get_recipes(serial_title = "Combinations", column_separator = "|", separator = "-
+                      text_position = "center", left_padding = 1, right_padding = 2, column_width = 0)
